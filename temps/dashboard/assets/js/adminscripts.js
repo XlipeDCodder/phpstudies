25ec876f6c40ea219825573faf408963342901ce
Felipe Santana D`Eça Almeida <felipe.almeida@saebti.intranet>
2023-07-20 11:54:44 -0300

    Dashboard details add

--- temps/dashboard/assets/js/adminscripts.js
+++ temps/dashboard/assets/js/adminscripts.js
@@ -10,3 +10,13 @@ function activelink(){
 }
 
 list.forEach(item => item.addEventListener("mouseover", activelink));
+
+// toggle section
+let toggle = document.querySelector(".toggle");
+let navigation = document.querySelector(".navigation");
+let main = document.querySelector(".main");
+
+toggle.onclick = function(){
+    navigation.classList.toggle("active");
+    main.classList.toggle("active");
+}
